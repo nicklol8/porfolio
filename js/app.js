473902ba5a4badaf8547dc2bed6eaf16dc722f0e
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-09-17 17:49:15 -0400

    work

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const openPokestop = () => {
+  const pokestopModal = document.getElementById('pokestop-modal');
+  pokestopModal.style.display = 'block';
+};
+// const span = document.getElementsByClassName('close');
+// span.onclick = function(event) {
+//   console.log(event.target);
+//   event.target.parent.style.display = 'none';
+// };
+// const closeModal = event => {
+//   console.log('click');
+// };
