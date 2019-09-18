ab40e800833ebe526bb7993c7e55fa6770553bcc
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-09-18 00:37:44 -0400

    work

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
-const openPokestop = () => {
+const pokestopModal = () => {
   const pokestopModal = document.getElementById('pokestop-modal');
-  pokestopModal.style.display = 'block';
+  if (pokestopModal.style.display === 'block') {
+    pokestopModal.style.display = 'none';
+  } else {
+    pokestopModal.style.display = 'block';
+  }
 };
-// const span = document.getElementsByClassName('close');
-// span.onclick = function(event) {
-//   console.log(event.target);
-//   event.target.parent.style.display = 'none';
-// };
-// const closeModal = event => {
-//   console.log('click');
-// };
+const recipebookModal = () => {
+  const recipeModal = document.getElementById('recipebook-modal');
+  if (recipeModal.style.display === 'block') {
+    recipeModal.style.display = 'none';
+  } else {
+    recipeModal.style.display = 'block';
+  }
+};
+const foodsoModal = () => {
+  const foodsoModal = document.getElementById('foodso-modal');
+  if (foodsoModal.style.display === 'block') {
+    foodsoModal.style.display = 'none';
+  } else {
+    foodsoModal.style.display = 'block';
+  }
+};
+const donutdashModal = () => {
+  const donutdashModal = document.getElementById('donutdash-modal');
+  if (donutdashModal.style.display === 'block') {
+    donutdashModal.style.display = 'none';
+  } else {
+    donutdashModal.style.display = 'block';
+  }
+};
